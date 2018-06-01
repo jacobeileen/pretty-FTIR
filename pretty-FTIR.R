7f0fbdc58993de0c479d053046a288ea6ffc9a7d
Eileen Jacob <38068748+jacobeileen@users.noreply.github.com>
2018-06-01 14:37:47 +0100

    "Generic" script

---
+++ pretty-FTIR.R
@@ -0,0 +1,44 @@
+library(ggplot2)
+library(plyr)
+library(dplyr)
+
+### Import and merge csv files ###
+
+# Specify folder with spectra files:
+setwd("~/directory_here")
+
+# Import and tidy:
+t <- list.files(pattern="*.csv")
+
+spectra <- lapply(t, function(i) {
+  read.csv(i, stringsAsFactors = FALSE)})
+
+names(spectra <- gsub("\\s1\\.csv|\\.csv", "", t))
+
+spectra <- lapply(spectra, "[", -(1:5), ) # Delete intro rows of each file
+
+ed <- bind_rows(spectra, .id = "Sample")
+ed$XLabel <- as.numeric(ed$XLabel)
+ed$Wavenumber <- as.numeric(ed$Wavenumber)
+
+
+### Factor and offset spectra ###
+
+ed$Sample <- factor(ed$Sample, levels(ed$Sample)[c("reorder spectra here")]) #order by factor level
+ed$offset <- as.integer(ed$Sample) / "offset factor here" #to 'stack' spectra
+ta <- ddply(ed, .(Sample), mutate, gr = Wavenumber + offset)
+
+### Plot spectra ###
+
+x <- ggplot(data = ta) +
+  geom_line(aes(XLabel, gr, group = (Sample), colour = Sample)) +
+  guides(colour = guide_legend(reverse=TRUE)) +
+  scale_x_reverse(lim = (c(4000,700)), breaks = seq(4000, 700, -500), name = "Wavenumber") +
+  scale_y_continuous(name = "Absorbance, arbitrary units", breaks = NULL, labels = NULL) +
+  theme_bw()
+
+### To plot particular spectra: x %+% subset(ta, Sample %in% c("list spectra here"))
+
+ggsave("file_name.pdf", width = 8.4, height = 6) #Save as pdf image
+
+### To extract individual spectra from list: list2env(spectra, envir = .GlobalEnv)
